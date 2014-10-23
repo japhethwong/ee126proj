3fb44de8912228e6d21a868c11d140a50480b695
Nishanth Mohan <nishanthmohan@tests-MacBook-Pro-4.local>
2014-10-22 22:33:30 -0700

    Whatevs

--- ramchandranNumber.py
+++ ramchandranNumber.py
@@ -4,7 +4,7 @@ from scrape import getCoAuthorsForAuthor, RAMCHANDRAN_NAME, RAMCHANDRAN_ID
 import sys
 
 PADDING = " " * 4
-ramchandran_numbers = {(RAMCHANDRAN_ID, RAMCHANDRAN_NAME) : 0}
+ramchandran_numbers = {RAMCHANDRAN_NAME : 0}
 
 
 INTIAL_ALPHA = 5
@@ -62,7 +62,7 @@ def _bfs_(Q):
 # Populates the initial databae of Ramchandran numbers
 def _populate_initial_database_():
 	Q = deque()
-	Q.append(((RAMCHANDRAN_ID, RAMCHANDRAN_NAME), 0))
+	Q.append((RAMCHANDRAN_NAME, 0))
 	_bfs_(Q)
 
 # Populates the database given a random author trying 
@@ -93,10 +93,10 @@ def test():
 	author = RAMCHANDRAN_NAME
 	while len(ramchandran_numbers) < 25:
 		print "Author", author
-		coauthors = get_coauthors(author, 10)
+		coauthors = get_coauthors(author, 1000)
 		print "Co-authors", coauthors
 		print "\n"
-		author = get_random_coauthor(coauthors)
+		author = get_random_coauthor(coauthors)[1]
 
 def run():
 	_populate_initial_database_()
@@ -105,12 +105,12 @@ def run():
 
 	entries = ramchandran_numbers.items()
 	entries.sort(key=lambda(elem): elem[1])
-	longest_author = len(max(entries, key=lambda(elem): len(elem[0][1]))[0][1])
+	longest_author = len(max(entries, key=lambda(elem): len(elem[0]))[0])
 
 	print("\nAUTHOR" + PADDING + " " * max(0, longest_author - 6) + "RAMCHANDRAN NUMBER")
 	print("")
 	for entry in entries:
-		author = unicode(entry[0][1]).title()
+		author = unicode(entry[0]).title()
 		spaces = " " * max(longest_author - len(author), 0)
 		number = str(entry[1])
 		print(author + spaces + PADDING + number)
